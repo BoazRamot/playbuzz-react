89ee655df1ec2b66c36c5dfa34882611d951a9df
BoazRamot <boaz.ramot@gmail.com>
2019-07-07 20:07:34 +0300

    Updates

---
+++ precache-manifest.1018a3eac1f3358f4923c98fb797661d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ba0d40db2e8918fa0dc281a11f8430",
+    "url": "/playbuzz-react/index.html"
+  },
+  {
+    "revision": "7723b4b86d3f644728f0",
+    "url": "/playbuzz-react/static/css/main.c0fecfc2.chunk.css"
+  },
+  {
+    "revision": "894dc235809f6fc71955",
+    "url": "/playbuzz-react/static/js/2.3e5fc9ee.chunk.js"
+  },
+  {
+    "revision": "7723b4b86d3f644728f0",
+    "url": "/playbuzz-react/static/js/main.9708046a.chunk.js"
+  },
+  {
+    "revision": "88016e83d9430edd2f38",
+    "url": "/playbuzz-react/static/js/runtime~main.a7f77c73.js"
+  },
+  {
+    "revision": "96be9e5916244732db08af03e8d08bcc",
+    "url": "/playbuzz-react/static/media/logo.96be9e59.png"
+  }
+]);
